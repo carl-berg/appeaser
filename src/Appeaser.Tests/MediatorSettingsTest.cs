f5d56e56b2a7f540c3ac5f28d95f715a09a985ee
Carl Berg <carl.berg@spinit.se>
2018-08-24 13:40:03 +0200

    Added some more exception tests

--- src/Appeaser.Tests/MediatorSettingsTest.cs
+++ src/Appeaser.Tests/MediatorSettingsTest.cs
@@ -15,29 +15,61 @@ namespace Appeaser.Tests
         }
 
         [Fact]
-        public void Test_Exception_Is_Wrapped_By_Default()
+        public void Test_Request_Exception_Is_Wrapped_By_Default()
         {
             var mediator = new Mediator(_handlerFactory);
             Assert.Throws<MediatorRequestException>(() => mediator.Request(new TestFeature.Request()));
         }
 
         [Fact]
-        public void Test_Exception_Wrapping_Can_Be_Disabled()
+        public void Test_Query_Exception_Is_Wrapped_By_Default()
+        {
+            var mediator = new Mediator(_handlerFactory);
+            Assert.Throws<MediatorQueryException>(() => mediator.Request(new TestFeature.Query()));
+        }
+
+        [Fact]
+        public void Test_Command_Exception_Is_Wrapped_By_Default()
+        {
+            var mediator = new Mediator(_handlerFactory);
+            Assert.Throws<MediatorCommandException>(() => mediator.Send(new TestFeature.Command()));
+        }
+
+        [Fact]
+        public void Test_Request_Exception_Wrapping_Can_Be_Disabled()
         {
             var mediator = new Mediator(_handlerFactory, new TestMediatorSettings { WrapExceptions = false });
             Assert.Throws<Exception>(() => mediator.Request(new TestFeature.Request()));
         }
 
+        [Fact]
+        public void Test_Query_Exception_Wrapping_Can_Be_Disabled()
+        {
+            var mediator = new Mediator(_handlerFactory, new TestMediatorSettings { WrapExceptions = false });
+            Assert.Throws<Exception>(() => mediator.Request(new TestFeature.Query()));
+        }
+
+        [Fact]
+        public void Test_Command_Exception_Wrapping_Can_Be_Disabled()
+        {
+            var mediator = new Mediator(_handlerFactory, new TestMediatorSettings { WrapExceptions = false });
+            Assert.Throws<Exception>(() => mediator.Request(new TestFeature.Command()));
+        }
+
         public class TestFeature
         {
             public class Request : IRequest<Result> { }
+            public class Query : IQuery<Result> { }
+            public class Command : ICommand<Result> { }
 
-            public class Handler : IRequestHandler<Request, Result>
+            public class Handler : 
+                IRequestHandler<Request, Result>,
+                IQueryHandler<Query, Result>,
+                ICommandHandler<Command, Result>
             {
-                public Result Handle(Request request)
-                {
-                    throw new Exception("Expected excepton");
-                }
+                public Result Handle(Request request) => throw new Exception("Expected excepton");
+                public Result Handle(Query query) => throw new Exception("Expected excepton");
+                public Result Handle(Command command) => throw new Exception("Expected excepton");
             }
 
             public class Result { }
