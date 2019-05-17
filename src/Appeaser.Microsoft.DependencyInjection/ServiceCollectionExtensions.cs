307092d4e82dffb2ab02c6474eac001ec8f5d21f
Carl Berg <carl.berg@spinit.se>
2019-05-17 14:36:09 +0200

    First implementation of microsoft dependencyinjection

---
+++ src/Appeaser.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Appeaser.Microsoft.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds Appeaser IMediator, ISimpleMediator and all handlers, in provided assemblies, to service collection
+        /// </summary>
+        /// <param name="assemblies">Assemblies containing handlers (if none provided, the calling assembly will be added by default)</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static IServiceCollection AddAppeaser(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            if (assemblies is null || !assemblies.Any())
+            {
+                assemblies = new[] { Assembly.GetCallingAssembly() };
+            }
+            return AddAppeaser(services, (MediatorSettings)null, assemblies);
+        }
+
+        /// <summary>
+        /// Adds Appeaser IMediator, ISimpleMediator and all handlers, in provided assemblies, to service collection
+        /// </summary>
+        /// <param name="assemblies">Assemblies containing handlers (if none provided, the calling assembly will be added by default)</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static IServiceCollection AddAppeaser(this IServiceCollection services, Action<MediatorSettings> configure, params Assembly[] assemblies)
+        {
+            if (assemblies is null || !assemblies.Any())
+            {
+                assemblies = new[] { Assembly.GetCallingAssembly() };
+            }
+            var settings = new MediatorSettings();
+            configure(settings);
+            return AddAppeaser(services, settings, assemblies);
+        }
+
+        /// <summary>
+        /// Adds Appeaser IMediator, ISimpleMediator and all handlers, in provided assemblies, to service collection
+        /// </summary>
+        /// <param name="assemblies">Assemblies containing handlers (if none provided, the calling assembly will be added by default)</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static IServiceCollection AddAppeaser(this IServiceCollection services, MediatorSettings settings, params Assembly[] assemblies)
+        {
+            if (assemblies is null || !assemblies.Any())
+            {
+                assemblies = new[] { Assembly.GetCallingAssembly() };
+            }
+
+            if (settings != null)
+            {
+                services.AddSingleton(settings);
+            }
+            
+            services.AddScoped<IMediatorResolver, MicrosoftDependencyInjectionMediatorResolver>();
+            services.AddScoped<IMediator, Mediator>();
+            services.AddScoped<ISimpleMediator, Mediator>();
+
+            var scanner = new TypeScanner(assemblies);
+
+            var handlers = scanner.ResolveOpenTypes(typeof(IRequestHandler<,>));
+            foreach (var handler in handlers)
+            {
+                services.AddTransient(handler.InterfaceType, handler.HandlerType);
+            }
+
+            var asyncHandlers = scanner.ResolveOpenTypes(typeof(IAsyncRequestHandler<,>));
+            foreach (var handler in asyncHandlers)
+            {
+                services.AddTransient(handler.InterfaceType, handler.HandlerType);
+            }
+
+            return services;
+        }
+    }
+}
---
+++ src/Appeaser.Microsoft.DependencyInjection/TypeScanner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Appeaser.Microsoft.DependencyInjection
+{
+    internal class TypeScanner
+    {
+        private readonly IEnumerable<Assembly> _assemblies;
+
+        public TypeScanner(IEnumerable<Assembly> assemblies)
+        {
+            _assemblies = assemblies;
+        }
+
+        public IEnumerable<ResolvedHandler> ResolveOpenTypes(Type openType)
+        {
+            foreach (var assembly in _assemblies)
+            {
+                foreach (var type in assembly.DefinedTypes)
+                {
+                    foreach (var @interface in type.GetInterfaces())
+                    {
+                        if (@interface.IsGenericType && openType.IsAssignableFrom(@interface.GetGenericTypeDefinition()))
+                        {
+                            yield return new ResolvedHandler(@interface, type.AsType());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        internal class ResolvedHandler
+        {
+            public ResolvedHandler(Type @interface, Type handler)
+            {
+                InterfaceType = @interface;
+                HandlerType = handler;
+            }
+
+            public Type InterfaceType { get; }
+            public Type HandlerType { get; }
+        }
+    }
+}
