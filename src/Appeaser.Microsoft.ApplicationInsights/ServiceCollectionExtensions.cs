60dda134e970bbced87ce0348937fc3a97ac03f2
Carl Berg <carl.berg@spinit.se>
2019-09-23 15:39:43 +0200

    Added Application Insights convenience projects and removed a dependency from Appeaser.Microsoft.DependencyInjection

---
+++ src/Appeaser.Microsoft.ApplicationInsights/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Appeaser.Diagnostics;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Appeaser.Microsoft.ApplicationInsights
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds a diagnostic interceptor and configures <see cref="DependencyTrackingTelemetryModule"/> to include it as a dependency.
+        /// </summary>
+        public static IServiceCollection ConfigureApplicationInsightsDiagnosticsForAppeaser(this IServiceCollection services, DependencyTrackingTelemetryModule depModule)
+        {
+            services.Configure<MediatorSettings>(cfg => cfg.AddInterceptor<DiagonsticInterceptor>());
+            depModule.IncludeDiagnosticSourceActivities.Add(DiagonsticInterceptor.DiagnosticSourceName);
+            return services;
+        }
+    }
+}
